1021a4200b64d746ae09d64db8c54b659c3bd3cb
shahvrushabh646-wq <shahvrushabh646@gmail.com>
2026-09-13 12:32:15 +0530

    Configure Artha Android application

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.artha.market"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.artha.market"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
