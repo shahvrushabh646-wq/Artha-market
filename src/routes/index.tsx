b476cc06504bf8a94e29d9e0b1b29e2ef5d185fe
shahvrushabh646-wq <shahvrushabh646@gmail.com>
2026-09-26 18:34:24 +0530

    Fix Gold discount field before Vercel deployment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -142,7 +142,7 @@ const fetchPreciousMetals = createServerFn({ method: "GET" }).handler(async (): 
     gold5yHigh10g,
     goldDiscount10Price10g,
     goldDiscount20Price10g,
-    goldDiscount30Price10Price10g: goldDiscount30Price10g,
+    goldDiscount30Price10g,
     goldDiscount40Price10g,
     silver5yHighKg,
     silver25PriceKg,
